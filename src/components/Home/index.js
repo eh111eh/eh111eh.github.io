c5bae100b7b138249fc52f376e797803d6ba81d6
Hwayeon Kang <hwayeonniii@gmail.com>
2024-09-06 03:44:53 +0900

    Add project in home page, change social icons position, resize the window, etc

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -47,10 +47,35 @@ const Home = () => {
           Resume
           </a>
           <p>
-          Born and raised in Seoul, Korea, I'm currently studying mathematical physics in Edinburgh, UK.
-          I took a gap year during the 2023/24 academic year to explore different fields and interests,
-          but honestly, I'm still figuring things out.
+          Born and raised in Seoul, Korea, currently studying mathematical physics in Edinburgh, UK.
+          I took a gap year during the 2023/24 academic year to explore different fields and interests.
+          During that time, I worked on machine learning, condensed matter physics, and multibody dynamics
+          as a developer and research intern in Germany, Singapore, and remotely from Korea.
           </p>
+
+          <p>
+          I’ve worked on multiple projects, including:
+          <ul>
+            <li><a href='https://summerofcode.withgoogle.com/programs/2024/projects/kPT4IkQI'>Implementing specific forces and torques</a><br />
+            - Implements three nonlinear force actuators-Duffing spring, Coulomb kinetic friction, and Hill's muscle model-into
+            SymPy (sympy.physics.mechanics) and adds their corresponding example models to the documentation.
+            </li>
+            <li><a href='https://firebasestorage.googleapis.com/v0/b/portfolio-bfe5b.appspot.com/o/mpea-crss-private.png?alt=media&token=e48b255d-b880-4a68-b33c-e32e7576c65d'>MPEA-CRSS-Prediction</a><br />
+            - Develop a GAN-based regression model and tree-based models to better predict critical resolved shear stress
+            from multi-principle element alloys and short range order data.<br />
+            - Currently not publicly available since the paper hasn’t been published yet.
+            </li>
+            <li><a href='https://docs.google.com/presentation/d/14_5BI1k6dh45tAGgxK2JH4x2ieat8ACB5CY66d3LZbQ/pub?start=false&loop=false&delayms=3000&slide=id.g279179e672e_4_965'>Identify range residency from animal tracking data using ML</a><br />
+            - Develop tree-based models to analyse animal range residency patterns from GPS datasets.<br />
+            - Currently not publicly available since the paper hasn’t been published yet.
+            </li>
+            <li><a href=''>LungLink Hub</a><br />
+            - A ML based web application to predict lung cancer mortality using socioeconomic and health data.
+            </li>
+          </ul>
+          You can also find these projects on the <a href='https://eh111eh.github.io/#/project'>Projects</a> page.
+          </p>
+
           <p>
           I love going for coffee — you can find some great spots in Edinburgh on my foodie account, @tastes.of.edinburgh.
           I'm also really into Economics.
