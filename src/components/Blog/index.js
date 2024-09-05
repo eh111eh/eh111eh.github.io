be2516987df8e20224eab5ca5ff1215c8fbff26e
Hwayeon Kang <hwayeonniii@gmail.com>
2024-09-06 04:15:38 +0900

    update

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -18,22 +18,22 @@ const Blog = () => {
   });
 
   const blogPosts = [
-    { title: 'GSoC 2024: Final Evaluation', date: '23 August 2024', link: '/blog/finalevaluation', external: false },
-    { title: 'GSoC 2024: Week 12', date: '18 August 2024', link: '/blog/week12', external: false },
-    { title: 'GSoC 2024: Week 11', date: '9 August 2024', link: '/blog/week11', external: false },
-    { title: 'GSoC 2024: Week 10', date: '4 August 2024', link: '/blog/week10', external: false },
-    { title: 'GSoC 2024: Week 9', date: '27 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week9.md', external: true },
-    { title: 'GSoC 2024: Week 8', date: '21 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week8.md', external: true },
-    { title: 'GSoC 2024: Week 7', date: '14 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week7.md', external: true },
-    { title: 'GSoC 2024: Mid Evaluation', date: '12 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/01-work-summary/mid-evaluation.md', external: true },
-    { title: 'GSoC 2024: Week 6', date: '7 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week6.md', external: true },
-    { title: 'GSoC 2024: Week 5', date: '30 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week5.md', external: true },
-    { title: 'GSoC 2024: Week 4', date: '23 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week4.md', external: true },
-    { title: 'GSoC 2024: Week 3', date: '16 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week3.md', external: true },
-    { title: 'GSoC 2024: Week 2', date: '9 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week2.md', external: true },
-    { title: 'GSoC 2024: Week 1', date: '2 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week1.md', external: true },
+    { title: 'GSoC Final Evaluation', date: '23 August 2024', link: '/blog/finalevaluation', external: false },
+    { title: 'GSoC Week 12', date: '18 August 2024', link: '/blog/week12', external: false },
+    { title: 'GSoC Week 11', date: '9 August 2024', link: '/blog/week11', external: false },
+    { title: 'GSoC Week 10', date: '4 August 2024', link: '/blog/week10', external: false },
+    { title: 'GSoC Week 9', date: '27 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week9.md', external: true },
+    { title: 'GSoC Week 8', date: '21 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week8.md', external: true },
+    { title: 'GSoC Week 7', date: '14 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week7.md', external: true },
+    { title: 'GSoC Mid Evaluation', date: '12 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/01-work-summary/mid-evaluation.md', external: true },
+    { title: 'GSoC Week 6', date: '7 July 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week6.md', external: true },
+    { title: 'GSoC Week 5', date: '30 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week5.md', external: true },
+    { title: 'GSoC Week 4', date: '23 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week4.md', external: true },
+    { title: 'GSoC Week 3', date: '16 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week3.md', external: true },
+    { title: 'GSoC Week 2', date: '9 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week2.md', external: true },
+    { title: 'GSoC Week 1', date: '2 June 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/week1.md', external: true },
     { title: 'Note: Hill-Type Muscle Model', date: '30 May 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/03-documents/notes.md', external: true },
-    { title: 'GSoC 2024: Community Bonding Period', date: '26 May 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/community-bonding-period.md', external: true }
+    { title: 'GSoC Community Bonding Period', date: '26 May 2024', link: 'https://github.com/eh111eh/gsoc-blog/blob/main/_docs/02-weeks/community-bonding-period.md', external: true }
   ];
 
   return (
