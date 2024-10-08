17035f11598669684e8284423270c04e01f6cf8f
Hwayeon Kang <hwayeonniii@gmail.com>
2024-10-08 15:14:38 +0100

    Wrote two blog posts
    posts for edinburgh award and programme rep

--- src/components/Posts/edinburghaward.js
+++ src/components/Posts/edinburghaward.js
@@ -4,13 +4,36 @@ import './posts.scss';
 const EdinburghAward = () => {
   return (
     <div className="evaluation-container">
-      <h1>Awarded Edinburgh Award (Work Experience)</h1>
+      <h1>Achieved the Edinburgh Award (Work Experience)</h1>
       <h3>Category: Student Activity</h3>
-      <p className="date">Written on </p>
+      <p className="date">Written on Monday, 30 Sep 2024, in JCMB.</p>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/portfolio-bfe5b.appspot.com/o/Hwayeon%20Kang%20Edinburgh%20Award(Work%20Experience).pdf?alt=media&token=e1bc0e25-7889-4339-b720-2c06ac908348"
+        alt="edinburgh-award-certificate"
+        className="cafe-image"
+      /><br/>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/portfolio-bfe5b.appspot.com/o/Badge-The%20University%20of%20Edinburgh%20The%20Edinburgh%20Award%20-%20Work%20Experience%20-%202024-09-27.png?alt=media&token=a93b0d24-ab01-4f87-bb49-f0d9ec218bee"
+        alt="edinburgh-award-badge"
+        className="cafe-image"
+      />
       <p>
-        Coming Soon!
+        I finally completed my journey to achieve the Edinburgh Award!<br/><br/><br/>
+
+        Throughout the award period, I focused on improving three development goals:
+        Oral Communication, Working with Others, and Finding Solutions, while
+        maximizing my impact through my work. As part of the award,
+        I volunteered to give a talk at the upcoming GSoC 2024 Lightning Talk,
+        which I am very much looking forward to.<br/><br/><br/>
+
+        This experience helped me recognize my skills and career goals and provided a
+        great opportunity to connect with other students working in different fields.
       </p>
-      
+      <p>
+        This award will soon appear on my HEAR (Higher Education Achievement Report),
+        and there will be a celebration event in November to recognize this achievement.
+      </p>
+
     </div>
   );
 };
--- src/components/Posts/programmerep1.js
+++ src/components/Posts/programmerep1.js
@@ -7,10 +7,59 @@ const ProgrammeRep1 = () => {
       <h1>Joined Students Association as a Programme Representative</h1>
       <h3>Category: Student Activity</h3>
       <p className="date">Written on Tuesday, 8 Oct 2024, in JCMB.</p>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/portfolio-bfe5b.appspot.com/o/Programme%20Representative%20Welcome.jpg?alt=media&token=6191bb01-5671-4278-ab4a-29dec92a3d61"
+        alt="welcome-email"
+        className="cafe-image"
+      />
       <p>
-        Coming Soon!
+        I've become a 2024/25 Year 3 Mathematical Physics Programme Representative! Being a rep and supporting the school has been
+        one of my biggest goals in college, so I'm very grateful for this opportunity. <br/><br/><br/>
+
+        My role is to represent fellow students in the third-year Mathematical Physics Programme.
+        I'll be working closely with other student representatives and leaders, including members of academic societies,
+        to ensure that any issues affecting students beyond our programme are effectively escalated and addressed.<br/><br/><br/>
+
+        The School of Physics and Astronomy has an upcoming SSLC (Student Staff Liaison Committee) meeting, so my fellow Year 3
+        physics reps and I are sending out online surveys to gather feedback on students' academic and social experiences this semester.
       </p>
-      
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/portfolio-bfe5b.appspot.com/o/Survey.png?alt=media&token=2de290bc-551b-44ce-94e9-7441b5a128c3"
+        alt="welcome-email"
+        className="cafe-image"
+      />
+
+      <p>
+        I also made an announcement at the beginning of the workshops to encourage students to complete the survey.
+      </p>
+
+      <p>
+        Since we’ve just set everything up and the timeframe is tight, we’re working at a fast pace. There have been some disagreements between
+        reps and ongoing challenges with the process. We'll be meeting tomorrow to address these issues, review the survey results,
+        and plan our next steps.
+      </p>
+
+      <p>
+        While working as a rep, I’ve found that my past work experiences have been very helpful. Although this role isn’t a scientific or technological one,
+        it requires a lot of communication, teamwork, and decision-making skills. I often have to decide whether to move forward with the current
+        ideas or wait for feedback from non-responsive peers, and then persuade them if they raise concerns after a decision has already been made.
+      </p>
+
+      <p>
+        Sometimes, a few of my peers become quite emotional, as if they’re disagreeing with me personally rather than just my idea.
+        I try not to focus too much on their emotions but instead concentrate on the ideas and evidence they present.
+        During my gap year, I learned that there are many different types of people in the workplace, each with their own way of
+        expressing and persuading their opinions.
+      </p>
+
+      <p>
+      Lastly, I’d like to end this post with something my friend told me when I told her I became a rep:
+      </p>
+
+      <p><em><strong>
+        With great power comes great responsibility!
+      </strong></em></p>
+
     </div>
   );
 };
